25e7be86675aa915262f0da983ae9b65b3f9f81a
kay1810 <mailkarthikan@gmail.com>
2019-08-08 11:54:47 +0530

    Update getinfo.ps1

--- getinfo.ps1
+++ getinfo.ps1
@@ -1,9 +1,3 @@
-$HTMLReport = "Report.html" 
-
 $ReportTitle = "System info"
+Get-PSDrive | ConvertTo-Html -Property Name,Used,Provider,Root,CurrentLocation -CssUri "table.css" -Title $ReportTitle -Body "<h1>$ReportTitle</h1>`n<h5>Updated: on $(Get-Date)</h5>" | Out-File Report.html
  
-$ResultSet = Get-PSDrive | ConvertTo-Html -Property Name,Used,Provider,Root,CurrentLocation -CssUri "table.css" -Title $ReportTitle -Body "<h1>$ReportTitle</h1>`n<h5>Updated: on $(Get-Date)</h5>"
- 
-Add-Content $HTMLReport $ResultSet 
-
-Invoke-Item $HTMLReport
